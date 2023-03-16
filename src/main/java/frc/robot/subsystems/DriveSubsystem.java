7b3f3992e972798e83feadaa2abc04e270cfe923
FRC 2852 <122186216+frc-2852@users.noreply.github.com>
2023-03-15 20:36:01 -0400

    Working distance to travel

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -5,6 +5,8 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.SparkMaxPIDController;
 import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
@@ -19,10 +21,21 @@ public class DriveSubsystem extends SubsystemBase {
 
   // Hardware
   private final CANSparkMax mLeftLeader, mRightLeader, mLeftFollower, mRightFollower;
-  private DifferentialDrive mDifferentialDrive;
+  private RelativeEncoder mLeftEncoder;
+  private SparkMaxPIDController mLeftPIDController;
+
+  private RelativeEncoder mRightEncoder;
+  private SparkMaxPIDController mRightPIDController;
+
+  private DifferentialDrive mDifferentialDrive = null;
   private final DoubleSolenoid mShifter;
 
   private boolean mIsHighGear;
+
+  private final double WHEEL_DIAMETER_INCHES = 6.0;
+  private final double GEAR_RATIO = 15.0;
+  private final double ENCODER_CONVERSION_FACTOR = (1 / (WHEEL_DIAMETER_INCHES * Math.PI)) * GEAR_RATIO;
+  private double mDistanceToTravelInches = 0;
 
   private void configureSpark(CANSparkMax sparkMax, boolean inverted) {
     sparkMax.setInverted(inverted);
@@ -33,9 +46,21 @@ public class DriveSubsystem extends SubsystemBase {
   }
 
   public DriveSubsystem() {
-    
+
     mLeftLeader = new CANSparkMax(Constants.DRIVE_LEFT_LEADER, MotorType.kBrushless);
     configureSpark(mLeftLeader, false);
+
+    mLeftPIDController = mLeftLeader.getPIDController();
+    mLeftPIDController.setP(0.1);
+    mLeftPIDController.setI(0);
+    mLeftPIDController.setD(0);
+    mLeftPIDController.setIZone(0);
+    mLeftPIDController.setFF(0);
+    mLeftPIDController.setOutputRange(-0.2, 0.2);
+
+    mLeftEncoder = mLeftLeader.getEncoder();
+    mLeftEncoder.setPosition(0);
+    mLeftEncoder.setPositionConversionFactor(ENCODER_CONVERSION_FACTOR);
 
     mLeftFollower = new CANSparkMax(Constants.DRIVE_LEFT_FOLLOWER, MotorType.kBrushless);
     configureSpark(mLeftFollower, false);
@@ -44,28 +69,61 @@ public class DriveSubsystem extends SubsystemBase {
     mRightLeader = new CANSparkMax(Constants.DRIVE_RIGHT_LEADER, MotorType.kBrushless);
     configureSpark(mRightLeader, false);
 
+    mRightPIDController = mRightLeader.getPIDController();
+    mRightPIDController.setP(0.1);
+    mRightPIDController.setI(0);
+    mRightPIDController.setD(0);
+    mRightPIDController.setIZone(0);
+    mRightPIDController.setFF(0);
+    mRightPIDController.setOutputRange(-0.2, 0.2);
+
+    mRightEncoder = mRightLeader.getEncoder();
+    mRightEncoder.setPosition(0);
+    mRightEncoder.setPositionConversionFactor(ENCODER_CONVERSION_FACTOR);
+
     mRightFollower = new CANSparkMax(Constants.DRIVE_RIGHT_FOLLOWER, MotorType.kBrushless);
     configureSpark(mRightFollower, false);
     mRightFollower.follow(mRightLeader);
-    
-    mShifter = new DoubleSolenoid(Constants.PNEUMATIC_HUB, PneumaticsModuleType.REVPH, Constants.DRIVE_GEAR_BOX_OPEN, Constants.DRIVE_GEAR_BOX_CLOSE);
+
+    mShifter = new DoubleSolenoid(Constants.PNEUMATIC_HUB, PneumaticsModuleType.REVPH, Constants.DRIVE_GEAR_BOX_OPEN,
+        Constants.DRIVE_GEAR_BOX_CLOSE);
 
     SetLowGear();
 
     mDifferentialDrive = new DifferentialDrive(mLeftLeader, mRightLeader);
+
+    mDistanceToTravelInches = 12 * 15;
   }
 
   @Override
   public void periodic() {
     SmartDashboard.putBoolean("High Gear", mIsHighGear);
+
+    //Bad auto
+    if (mDistanceToTravelInches > 0) {
+      double distancePosition = mDistanceToTravelInches * 0.27; // 3.24
+      double distance = distancePosition - mLeftEncoder.getPosition();
+      if (distance > 0.5) {
+        mDifferentialDrive.tankDrive(0.39, -0.4);
+      } else {
+        mDifferentialDrive.tankDrive(0, 0);
+        mDistanceToTravelInches = 0;
+      }
+    }
+  }
+
+  public void DriveForwardInches(double distanceToTravelInches) {
+    mDistanceToTravelInches = distanceToTravelInches;
   }
 
   public void ArcadeDrive(double xSpeed, double zRotation) {
-		mDifferentialDrive.arcadeDrive(xSpeed, zRotation);
-	}
+    if (mDistanceToTravelInches == 0) {
+      mDifferentialDrive.arcadeDrive(xSpeed, zRotation);
+    }
+  }
 
   public void ToggleGear() {
-    if(mIsHighGear){
+    if (mIsHighGear) {
       SetLowGear();
     } else {
       SetHighGear();
